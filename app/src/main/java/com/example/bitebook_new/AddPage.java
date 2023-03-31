70522eda87deeeb8dfcbc9c9f4f9e8d491f6da45
ChewonHan <about0322@naver.com>
2023-03-31 20:52:40 +0800

    Layout for AddPage

--- app/src/main/java/com/example/bitebook_new/AddPage.java
+++ app/src/main/java/com/example/bitebook_new/AddPage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,9 +14,19 @@ public class AddPage extends Fragment {
         // require a empty public constructor
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_page, container, false);
+
+
     }
 }
