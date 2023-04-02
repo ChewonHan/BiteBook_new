3b6e24ec7d1fe34997bb15cc30e8f026e1170c6f
ChewonHan <about0322@naver.com>
2023-04-02 10:34:19 +0800

    Added Tags to the homePage

--- app/src/main/java/com/example/bitebook_new/HomePage.java
+++ app/src/main/java/com/example/bitebook_new/HomePage.java
@@ -1,12 +1,15 @@
 package com.example.bitebook_new;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,8 +22,10 @@ public class HomePage extends Fragment {
     RecyclerView homeRecycler;
     RecyclerView.Adapter adapter;
     ArrayList<String> items;
-
     ArrayList<Entry> entries;
+    Spinner areaSpinner;
+    Spinner cuisineSpinner;
+    Spinner priceSpinner;
 
 
     public HomePage()
@@ -29,12 +34,36 @@ public class HomePage extends Fragment {
     }
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
         items = new ArrayList<>();
         items.add("First item");
         items.add("se item");
         items.add("s item");
+
+        areaSpinner = view.findViewById(R.id.areaSpinner);
+        cuisineSpinner = view.findViewById(R.id.cuisineSpinner);
+        priceSpinner = view.findViewById(R.id.priceSpinner);
+
+        // set up the spinners
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                getContext(), R.array.areaSpinner, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        areaSpinner.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                getContext(), R.array.cuisineSpinner, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cuisineSpinner.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+                getContext(), R.array.priceSpinner, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priceSpinner.setAdapter(adapter3);
+
 
         entries = new ArrayList<>();
 
@@ -48,10 +77,6 @@ public class HomePage extends Fragment {
 
 
 
-
-
-
-        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
 //        ImageView imageView = (ImageView) view.findViewById(R.id.my_image);
         homeRecycler = (RecyclerView) view.findViewById(R.id.homeRecycler);
 
