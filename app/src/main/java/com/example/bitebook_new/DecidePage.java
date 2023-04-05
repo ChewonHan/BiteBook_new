28c4e1de2cadd70b01d9bfd3a73c712dbcb822cb
ChewonHan <about0322@naver.com>
2023-04-06 02:54:43 +0800

    Layout for decide Page - due to current empty recyclerView, generate Button is not shown

--- app/src/main/java/com/example/bitebook_new/DecidePage.java
+++ app/src/main/java/com/example/bitebook_new/DecidePage.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import java.io.*;
 
 public class DecidePage extends Fragment {
+
+    Spinner areaSpinner;
+    Spinner cuisineSpinner;
+    Spinner priceSpinner;
 
     public DecidePage(){
         // require a empty public constructor
@@ -16,7 +22,28 @@ public class DecidePage extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
 
-        return inflater.inflate(R.layout.fragment_decide_page, container, false);
+        areaSpinner = view.findViewById(R.id.areaSpinner);
+        cuisineSpinner = view.findViewById(R.id.cuisineSpinner);
+        priceSpinner = view.findViewById(R.id.priceSpinner);
+
+        // set up the spinners
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                getContext(), R.array.areaSpinner, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        areaSpinner.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                getContext(), R.array.cuisineSpinner, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cuisineSpinner.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+                getContext(), R.array.priceSpinner, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priceSpinner.setAdapter(adapter3);
+
+        return view;
     }
 }
