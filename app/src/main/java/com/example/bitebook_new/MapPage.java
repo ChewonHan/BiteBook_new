ef279ab951e07d33ae7845c46a41153c4e01d96a
ChewonHan <about0322@naver.com>
2023-04-10 12:03:55 +0800

    can see the bottom nav bar on map page

--- app/src/main/java/com/example/bitebook_new/MapPage.java
+++ app/src/main/java/com/example/bitebook_new/MapPage.java
@@ -2,13 +2,18 @@ package com.example.bitebook_new;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,16 +25,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MapPage extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap gMap;
     private MapView mapView;
+    Context context;
 
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+        context = container.getContext();
+
         // Get a reference to the MapView and set it up
         mapView = view.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
@@ -41,19 +51,9 @@ public class MapPage extends Fragment implements OnMapReadyCallback {
             mapView.getMapAsync(this);
         }
 
+
         return view;
     }
-
-
-//        @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_map);
-//        // Obtain the SupportFragment and get notified when the nap is ready to be used
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-////        mapFragment.getMapAsync( onMapReadyCallback.this);
-//    }
 
 
     @Override
